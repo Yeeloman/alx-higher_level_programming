c18235906b6ec061a66becdbdaf718120f49733b
ThickDepression <yami.onlyme@gmail.com>
2023-08-08 18:16:05 +0100

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        if (ord(str[i]) >= 97) and (ord(str[i]) <= 122):
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print("{}".format(str[i]), end="")
+        if i == len(str) - 1:
+            print()
