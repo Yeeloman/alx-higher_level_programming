cfeddfbe7198bdd9fe4f1080b7b184d21a302678
ThickDepression <yami.onlyme@gmail.com>
2023-08-08 18:57:41 +0100

    101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     if n >= 0:
         str_copy = str[:n] + str[n + 1:]
     else:
-        str_copy = str[:n] + str[n - 1:]
+        str_copy = str[:]
     return str_copy
