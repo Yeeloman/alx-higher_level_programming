1329e58e0195e4b18cf486d0aaeef07fdf6dc885
ThickDepression <yami.onlyme@gmail.com>
2023-08-10 05:32:25 +0100

    fix 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,4 @@
-#!/usr/#!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-
-fizzbuzz()
-print("")
-bin/python3
+#!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 5 == 0) and (i % 3 == 0):
