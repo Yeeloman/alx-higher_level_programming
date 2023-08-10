b463829bd1f4c2ba8cdc81fdd1f8733f0ab33025
ThickDepression <yami.onlyme@gmail.com>
2023-08-10 16:10:29 +0100

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
-
-_len = len(sys.argv)
-result = 0
-if _len == 1:
-    print("{:d}".format(result))
-else:
-    for i in range(1, _len):
-        result += int(sys.argv[i])
-    print("{:d}".format(result))
+    _len = len(sys.argv)
+    result = 0
+    if _len == 1:
+        print("{:d}".format(result))
+    else:
+        for i in range(1, _len):
+            result += int(sys.argv[i])
+        print("{:d}".format(result))
