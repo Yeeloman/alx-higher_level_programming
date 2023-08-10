ebf3203e86f8ecc0f21dd4e053e304e27ea63f92
ThickDepression <yami.onlyme@gmail.com>
2023-08-10 16:24:58 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    _len = len(sys.argv)
-    if _len == 1:
+    _len = len(sys.argv) - 1
+    if _len == 0:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(_len - 1))
-        for i in range(1, len(sys.argv)):
+        print("{} arguments:".format(_len))
+        for i in range(1, _len + 1):
             print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     _len = len(sys.argv)
     result = 0
     for i in range(1, _len):
         result += int(sys.argv[i])
-    print("{:d}".format(result))
+    print(result)
