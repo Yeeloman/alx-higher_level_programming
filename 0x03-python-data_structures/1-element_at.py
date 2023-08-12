790edf0f4b7e6bccaa2045a30e9e356d06438e23
ThickDepression <yami.onlyme@gmail.com>
2023-08-12 03:10:12 +0100

    add

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return None
-    list = my_list.copy()
-    value = list.pop(idx)
-    return value
+    return my_list[idx]
