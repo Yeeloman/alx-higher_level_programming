a0094c4edac2e039d17baee98fccda1fda85da86
ThickDepression <yami.onlyme@gmail.com>
2023-08-14 18:12:17 +0100

    add

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ *is_palindrome - checks if the list is is_palindrome
+ *@head: head of the list
+ *Return: returns 1 if yes
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *list = *head;
+	int i = 0, start, end;
+	int palind[100] = {0};
+
+	while (list)
+	{
+		palind[i] = list->n;
+		list = list->next;
+		i++;
+	}
+	for (start = 0, end = i - 1; start < end; start++, end--)
+	{
+		if (palind[start] != palind[end])
+			return (0);
+	}
+	return (1);
+}
