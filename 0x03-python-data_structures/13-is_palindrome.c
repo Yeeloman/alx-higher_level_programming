9cbacc6999fa04f3c33716532ed3dce1d97a82a7
ThickDepression <yami.onlyme@gmail.com>
2023-08-14 18:33:09 +0100

    add

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  *is_palindrome - checks if the list is is_palindrome
@@ -11,8 +12,18 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *list = *head;
 	int i = 0, start, end;
-	int palind[100] = {0};
+	int *palind = NULL;
 
+	while (list)
+	{
+		list = list->next;
+		i++;
+	}
+	palind = (int *)malloc(i * sizeof(int));
+	if (palind == NULL)
+		return (0);
+	list = *head;
+	i = 0;
 	while (list)
 	{
 		palind[i] = list->n;
@@ -22,7 +33,11 @@ int is_palindrome(listint_t **head)
 	for (start = 0, end = i - 1; start < end; start++, end--)
 	{
 		if (palind[start] != palind[end])
+		{
+			free(palind);
 			return (0);
+		}
 	}
+	free(palind);
 	return (1);
 }
