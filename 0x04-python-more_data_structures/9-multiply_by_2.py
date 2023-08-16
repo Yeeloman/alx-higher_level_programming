aacfcfcb04ee25a57fa74e805d1b4db8bbdeabb9
ThickDepression <yami.onlyme@gmail.com>
2023-08-16 20:15:26 +0100

    add

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
-    listkeys = list(new_dic.key())
+    listkeys = list(new_dic.keys())
     for i in listkeys:
         new_dic[i] *= 2
     return new_dic
