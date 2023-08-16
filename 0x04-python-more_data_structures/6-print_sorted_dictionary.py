47cc8161bfa9b7722394c3c857a7e8519bdff017
ThickDepression <yami.onlyme@gmail.com>
2023-08-16 20:05:34 +0100

    add

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-    sort = sorted(a_dictionary)
-    for key in sort:
-        print("{:s} : {}".format(key, a_dictionary[key]))
+    sorted_list = list(a_dictionary.keys())
+    sorted_list.sort()
+    for item in sorted_list:
+        print("{}: {}".format(item, a_dictionary.get(item)))
