9104964688702105c65c72055af8587708d89608
ThickDepression <yami.onlyme@gmail.com>
2023-08-29 19:28:37 +0100

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,12 +29,11 @@ class Square:
     Args:
     value (int): square size.
     """
+        self.__size = value
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
 
     @property
     def position(self):
