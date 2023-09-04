fe66d90ccf4fe7e4565d4bf187eba12dfef91fca
ThickDepression <yami.onlyme@gmail.com>
2023-09-04 02:51:40 +0100

    add

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -7,13 +7,13 @@
  */
 void print_python_string(PyObject *p)
 {
+	printf("[.] string object info\n");
 	if (PyUnicode_Check(p))
 	{
-		printf("[.] string object info\n");
 		if (PyUnicode_IS_COMPACT_ASCII(p))
 			printf("  type: %s\n", "compact ascii");
 		else
-			printf("  type: %s\n", "not compact ascii");
+			printf("  type: %s\n", "compact unicode object");
 		printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
 		printf("  value: %s\n", PyUnicode_AsUTF8(p));
 	}
