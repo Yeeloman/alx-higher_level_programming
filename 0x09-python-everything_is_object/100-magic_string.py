5aad3e76f60f9411619a81ed920094696555bb51
ThickDepression <yami.onlyme@gmail.com>
2023-09-05 16:10:03 +0100

    add

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n=0):
-    n += 1
-    return ("BestSchool, " * (n - 1) + "BestSchool")
+def magic_string():
+    magic_string.call_count = getattr(magic_string, 'call_count', 0) + 1
+    return ("BestSchool, " * (magic_string.call_count - 1) + "BestSchool")
