186235bfc737b44b418e8215bd6c9ca58d7d683f
ThickDepression <yami.onlyme@gmail.com>
2023-09-12 00:29:15 +0100

    add

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,14 +2,9 @@
 """dds a new attribute"""
 
 
-def can_add_attribute(obj):
-    """Check if an object allows dynamic attribute assignment."""
-    return hasattr(obj, '__dict__') or hasattr(type(obj), '__dict__')
-
-
 def add_attribute(obj, attribute, value):
-    """Adds a new attribute to an object if it's possible."""
-    if can_add_attribute(obj):
-        setattr(obj, attribute, value)
-    else:
+    """Adds a new attribute"""
+    if '__dict__' not in dir(obj) or '__slots__' in dir(obj):
         raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attribute, value)
