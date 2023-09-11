616505371d5a9a6fdd47ff8d63e69bfd74fa2157
ThickDepression <yami.onlyme@gmail.com>
2023-09-12 00:22:22 +0100

    add advanced

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""dds a new attribute"""
+
+
+def add_attribute(obj, attr, value):
+    """add_attribute.
+
+    :param obj:
+    :param attr:
+    :param value:
+    """
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attr")
+    if '__slots__' in dir(obj):
+        raise TypeError("can't add new attr")
+    else:
+        setattr(obj, attr, value)
