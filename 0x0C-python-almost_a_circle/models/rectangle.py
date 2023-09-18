9dc350fd0175715ea8ab241b72972eafb56c4d44
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-18 20:41:53 +0100

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -140,7 +140,7 @@ class Rectangle(Base):
         return attr_dic
 
     def __str__(self):
-        """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
+        """__str__."""
+        return f"[Rectangle] ({self.id}) \
+{self.x}/{self.y} - \
+{self.width}/{self.height}"
