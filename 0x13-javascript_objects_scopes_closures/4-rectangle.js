e4f363fe05a7a0d9ef969bf7eb0d678dc53bf6b6
Yeeloman <yami.onlyme@gmail.com>
2023-11-14 18:50:52 +0100

    add task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,19 +1,29 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && !Number.isNaN(w) && h > 0 && !Number.isNaN(h)) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let result = "";
+      let result = '';
       for (let j = 0; j < this.width; j++) {
-        result += "X";
+        result += 'X';
       }
       console.log(result);
     }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
   }
 };
