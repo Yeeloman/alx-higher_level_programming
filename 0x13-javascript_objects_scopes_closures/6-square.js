6c47edfb0ce35dc8c09ec8e6a53a1c369e4f266c
Yeeloman <yami.onlyme@gmail.com>
2023-11-14 19:09:02 +0100

    add task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let result = '';
+        for (let j = 0; j < this.width; j++) {
+          result += c;
+        }
+        console.log(result);
+      }
+    }
+  }
+};
