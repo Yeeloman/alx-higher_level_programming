34dc4e331bd1f7fa930ea3c7834c128f594a0e25
Yeeloman <yami.onlyme@gmail.com>
2023-11-19 22:21:55 +0100

    fix task 1 with adding binary to the query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,18 +5,19 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
+    conn = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306
     )
-    cur = db.cursor()
+    cur = conn.cursor()
     cur.execute(
-        """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id""")
+        """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id"""
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
