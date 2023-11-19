2babf29069b8a29bc077333e03a0eb51d60ac121
Yeeloman <yami.onlyme@gmail.com>
2023-11-19 23:04:24 +0100

    fix smtg

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a City
-"""
+"""contains the class definition of a City."""
+
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
