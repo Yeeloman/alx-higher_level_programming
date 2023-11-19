cb673391c5fa68cfd2c9cd5182606c30990ccc7e
Yeeloman <yami.onlyme@gmail.com>
2023-11-19 23:06:01 +0100

    fix smtg

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """contains the class definition of a City."""
 
-from model_state import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import ForeignKey
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """
     Class that defines each city
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states_id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
