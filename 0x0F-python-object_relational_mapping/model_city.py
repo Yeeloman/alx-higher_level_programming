a42f63ef186790a6ce66dc71ebcfd68855d0d075
Yeeloman <yami.onlyme@gmail.com>
2023-11-19 23:02:38 +0100

    test

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""contains the class definition of a City."""
-
-from sqlalchemy import MetaData, Column, Integer, String
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import ForeignKey
-
-mt_data = MetaData()
-Base = declarative_base(metadata=mt_data)
 
 
 class City(Base):
     """
     Class that defines each city
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states_id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
