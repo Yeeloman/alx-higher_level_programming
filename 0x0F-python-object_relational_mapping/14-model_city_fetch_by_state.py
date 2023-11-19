4079460cedfecefc696d3a5e5ba6a0109cea3521
Yeeloman <yami.onlyme@gmail.com>
2023-11-19 22:49:07 +0100

    fix states instead of state_id

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
+"""prints all City objects from the database hbtn_0e_14_usa"""
+
 from model_state import Base, State
-from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from model_city import City
+import sys
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    usrname = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            usrname,
+            passwd,
+            db_name
+        ))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
+    for instance in (session.query(
+        State.name,
+        City.id,
+        City.name
+    ).filter(State.id == City.state_id)):
+        print(f"{instance[0]}: ({str(instance[1])}) {instance[2]}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -16,4 +16,4 @@ class City(Base):
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state_id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states_id"), nullable=False)
