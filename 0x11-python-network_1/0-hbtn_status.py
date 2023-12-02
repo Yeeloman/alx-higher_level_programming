8db5157dbe6d9a98fe45ffe1e281af2b7dd6b242
Yeeloman <yami.onlyme@gmail.com>
2023-12-02 01:54:56 +0100

    all tasks

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that fetches
+https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print("Body response:")
+        print("    - type:", type(content))
+        print("    - content:", content)
+        print("    - utf8 content:", utf8_content)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+
+import requests as reqs
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1]
+    if not letter:
+        json_data = {'q': ""}
+    else:
+        json_data = {'q': letter}
+    try:
+        resp = reqs.post(url, data=json_data)
+        resp.raise_for_status()
+        js_data = resp.json()
+        if js_data == {}:
+            print("No result")
+        else:
+            print(f"[{js_data.get('id')}] {js_data.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
