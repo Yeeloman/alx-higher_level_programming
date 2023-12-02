ea39a944b62cd19f296f05f5209ce6e467339415
Yeeloman <yami.onlyme@gmail.com>
2023-12-02 02:06:45 +0100

    fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,11 +8,8 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = sys.argv[1]
-    if not letter:
-        json_data = {'q': ""}
-    else:
-        json_data = {'q': letter}
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    json_data = {"q": letter}
     try:
         resp = reqs.post(url, data=json_data)
         resp.raise_for_status()
